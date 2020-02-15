e0b49a56b12285add6488cfb1bff52b50cf6f4b7
Jesus Lozano <jalozano841@gmail.com>
2020-02-15 15:23:05 -0600

    Added NavBar and Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,27 @@
 <template>
-  <div class="home">
-    <h1>Hello There!</h1>
+    <div class="relative h-screen w-screen bg-cover bg-center shadow-2xl" 
+    :style="`background-image:url(${bgImage})`">
+    <div class="h-screen w-screen bg-red-800 opacity-75" />
+    <div class="absolute h-screen w-screen top-0 flex flex-col justify-center items-center">
+      <h1 class="text-6xl font-bold text-white hover:bg-white hover:text-red-800 opacity-75" >
+        Jesus Lozano
+      </h1>
+      <h3 class="text-3xl font-bold text-white px-4 hover:bg-white hover:text-red-800 opacity-75">
+        Student
+      </h3>
+    </div>
   </div>
 </template>
 
 <script>
+import bgImage from "../assets/bg.jpg"
+
 export default {
-  name: "Home"
+  name: "Home",
+  data() {
+    return {
+      bgImage
+    }
+  }
 };
 </script>
