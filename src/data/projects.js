36630b8a4b5ba00c15795cd4f30ed13f59eed717
Jesus Lozano <jalozano841@gmail.com>
2020-02-23 17:59:36 -0600

    Added mynotes

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,6 +3,7 @@ import todob from "../assets/projects/05.png"
 import todot from "../assets/projects/04.png"
 import inventory from "../assets/projects/03.png"
 import vuetodo from "../assets/projects/02.png"
+import mynotes from "../assets/projects/06.png"
 
 const projects = [
     {
@@ -39,6 +40,13 @@ const projects = [
         image: vuetodo,
         hashtags: ["todo", "vuejs", "javascript", "javascript"],
         url: "https://vuetodo.jlozan38.xyz/"
+    },
+    {
+        name: "Look at my Notes",
+        description: "A Vue.js application with Vuex, implementing a CRUD Notes app",
+        image: mynotes,
+        hashtags: ["crud", "vuejs", "javascript", "tailwind", "vuex", "vue-router"],
+        url: "https://mynotes.jlozan38.xyz/"
     }
 ]
 
